b5cb3863c38804e24513f652baee2d468b771daa
suryansh16yadav16-oss <suryansh16yadav.16@gmail.com>
2026-06-19 21:37:07 +0530

    Added custom_card.dart

---
+++ custom_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final Color color;
+  final Widget route;
+
+  const CustomCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.color,
+    required this.route,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => route),
+          );
+        },
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color, color.withOpacity(0.7)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 48, color: Colors.white),
+              const SizedBox(height: 12),
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
