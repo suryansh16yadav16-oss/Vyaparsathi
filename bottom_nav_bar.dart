7ab4011651a813a0810395eb0fbc5ebc6deeccff
suryansh16yadav16-oss <suryansh16yadav.16@gmail.com>
2026-06-19 22:12:13 +0530

    Added bottom_nav_bar.dart

---
+++ bottom_nav_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: const Color(0xFF2ECC71),
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.inventory), label: 'Inventory'),
+        BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Sales'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+      ],
+    );
+  }
+}
